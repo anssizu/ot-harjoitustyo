9776c7a7b10661d3fc9f88a56975b6a11d9fd88a
Anselmus Heino <anssizu@users.noreply.github.com>
2022-11-15 21:06:05 +0200

    JUnit testit ja kassapäätteen 100% testauskattavuus

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -7,14 +7,44 @@ import org.junit.Test;
 public class MaksukorttiTest {
 
     Maksukortti kortti;
+    int alkusaldo = 100;
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(alkusaldo);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void kortinAlkusaldoOnOikein() {
+        assertEquals(alkusaldo, kortti.saldo());
+    }
+    
+    @Test
+    public void saldonLatausToimii() {
+        int lisays = 100;
+        kortti.lataaRahaa(lisays);
+        assertEquals(alkusaldo + lisays, kortti.saldo());
+    }
+    
+    @Test
+    public void saldonOttaminenRiittavallaMaaralla() {
+        boolean tulos = kortti.otaRahaa(alkusaldo);
+        assertEquals(true, tulos);
+    }
+    
+    @Test
+    public void saldonOttaminenRiittamattomallallaMaaralla() {
+        kortti.otaRahaa(alkusaldo + 100);
+        assertEquals(alkusaldo, kortti.saldo());
+    }
+    
+    @Test
+    public void toStringTesti() {
+        assertEquals("saldo: 1.0", kortti.toString());
     }
 }
